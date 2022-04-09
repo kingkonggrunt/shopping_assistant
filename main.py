72752c74eef62b53e2650a26a81c84a09a7ebdd1
kingkonggrunt <little-kk@hotmail.com>
2022-04-10 01:37:17 +1000

    feat: v2

--- main.py
+++ main.py
@@ -32,18 +32,36 @@ def get_milk():
 #     return start
     
     
+def coles_item(next=None):
+    url = "https://shop.coles.com.au/a/national/everything/search/"
+    print("Open to Searching for Products")
+    while True:
+        search_item = (yield)
+        
+        options = Options()
+        options.headless = True
+        driver = webdriver.Firefox(options=options,
+                                executable_path=r"/home/mint/Desktop/Development/shopping_assistant/gecko/geckodriver")
+    
+        driver.get(f"{url}{search_item}")
+
+        if next: next.send(driver.page_source)
+
+        driver.quit()
+    
 # @coroutine
-def filter_search_result(filter: ColesFilter, target):
-    print("Accepting Search Results")
+def filter_search_result(filter: ColesFilter, next=None):
+    print(f"Open for Filtering Products. Filter: {filter}")
     while True:
         page_source: str = (yield)
         soup = BeautifulSoup(page_source, 'html.parser',
                              parse_only=filter())
-        target.send(soup)
+
+        if next: next.send(soup)
 
 # @coroutine     
-def identify_products():
-    print("Open for finding products")
+def identify_products(next=None):
+    print("Open for Identifying Products")
     product_count = 0
     while True:
         soup: BeautifulSoup = (yield)
@@ -59,42 +77,39 @@ def identify_products():
                 id=header.find("h3", class_="product-title")['data-itemid'],
                 link=header.find("a", class_="product-image-link")['href'],
                 partnumber=header.find("h3", class_="product-title")['data-partnumber']
-                
-                
             )
+                    
+            if next: next.send(item)
             
-            print(item)
-            product_count += 1
-            print(product_count)
+def printer(next=None):
+    while True:
+        _ = (yield)
+        print(_)
 
 def main():
     # get_milk()
     
-    printer = identify_products()
+    p = printer()
+    identifier = identify_products(next=p)
+    everyday = filter_search_result(ColesFilter.AllProducts, next=identifier)
+    item_search = coles_item(next=everyday)
     
-    everyday = filter_search_result(ColesFilter.New, printer)
     
-    next(printer)
+    next(item_search)
+    next(identifier)
     next(everyday)
+    next(p)
     
-    with open("stores/coles/milk.html", "r") as milk_page:
-        everyday.send(milk_page)
+    # with open("stores/coles/milk.html", "r") as milk_page:
+    #     everyday.send(milk_page)
         
-
+    while True:
+        item = input("What Coles Item are you looking for? ")
+        if item: item_search.send(item)
+        
             
             
             
-            
-        
-        
-        
-        
-        
-        
-        
-        
-        
-    
 
 if __name__ == "__main__":
     main()
