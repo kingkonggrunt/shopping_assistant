b7e92c18e65584a883efb19e55b63b7bcda97902
kingkonggrunt <little-kk@hotmail.com>
2022-04-09 23:03:37 +1000

    feat: v1

--- main.py
+++ main.py
@@ -1,41 +1,100 @@
-from flask import Flask
-from flask import request, render_template
-from markupsafe import escape
 
-app = Flask(__name__)
 
-@app.route("/")
-def index():
-    return "Index Page"
+from selenium import webdriver
+from selenium.webdriver.firefox.options import Options
 
-@app.route("/hello")
-def hello_world():
-    return "<p>Hello, World!</p>"
 
-@app.route("/<name>")
-def hello(name):
-    return f"Hello {escape(name)}!"
+from bs4 import BeautifulSoup, SoupStrainer
 
-@app.route("/post/<int:post_id>")
-def post(post_id):
-    return f"Post {escape(post_id)}!"
+from coles import ColesItem
+from filters import ColesFilter
 
-@app.route("/path/<path:subpath>")
-def show_subpath(subpath):
-    return f"Path {escape(subpath)}!"
+def get_milk():
+    url = "https://shop.coles.com.au/a/national/everything/search/milk"
 
-@app.route("/projects/")
-def projects():
-    return "The projects page"
-
-@app.route("/login", methods=["GET", "POST"])
-def login():
-    if request.method == "POST":
-        return "POST LOGIN"
-    else:
-        return "GET LOGIN"
+    options = Options()
+    # options.headless = True
+    driver = webdriver.Firefox(options=options,
+                               executable_path=r"/home/mint/Desktop/Development/shopping_assistant/gecko/geckodriver")
+    driver.get(url)
     
-@app.route("/bye/")
-@app.route("/bye/<name>")
-def bye(name=None):
-    return render_template("bye.html", name=name)
+    with open('stores/coles/milk.html', 'w') as f:
+        f.write(driver.page_source)
+
+    driver.quit()
+    
+    
+# def coroutine(func):
+#     def start(*args,**kwargs):
+#         cr = func(*args,**kwargs)
+#         cr.next()
+#         return cr
+#     return start
+    
+    
+# @coroutine
+def filter_search_result(filter: ColesFilter, target):
+    print("Accepting Search Results")
+    while True:
+        page_source: str = (yield)
+        soup = BeautifulSoup(page_source, 'html.parser',
+                             parse_only=filter())
+        target.send(soup)
+
+# @coroutine     
+def identify_products():
+    print("Open for finding products")
+    product_count = 0
+    while True:
+        soup: BeautifulSoup = (yield)
+        for header in soup.find_all("header", class_="product-header"):
+            item = ColesItem(
+                full_name=header.find('span', class_="accessibility-inline").text,
+                brand=header.find('span', class_="product-brand").text,
+                name=header.find('span', class_="product-name").text,
+                size=header.find('span', class_="package-size").text,
+                dollar=header.find('span', class_="dollar-value").text,
+                cent=header.find('span', class_="cent-value").text,
+                unit_price=header.find('span', class_="package-price").text,
+                id=header.find("h3", class_="product-title")['data-itemid'],
+                link=header.find("a", class_="product-image-link")['href'],
+                partnumber=header.find("h3", class_="product-title")['data-partnumber']
+                
+                
+            )
+            
+            print(item)
+            product_count += 1
+            print(product_count)
+
+def main():
+    # get_milk()
+    
+    printer = identify_products()
+    
+    everyday = filter_search_result(ColesFilter.New, printer)
+    
+    next(printer)
+    next(everyday)
+    
+    with open("stores/coles/milk.html", "r") as milk_page:
+        everyday.send(milk_page)
+        
+
+            
+            
+            
+            
+        
+        
+        
+        
+        
+        
+        
+        
+        
+    
+
+if __name__ == "__main__":
+    main()
